72078afe91ded127efb82a52eac01c4807b29b71
Bill La Forge <laforge49@gmail.com>
2021-06-18 13:22:29 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject asynchronous-entities "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "A universal persistent data structure"
+  :url "https://github.com/laforge49/asynchronous-entities"
+  :license {:name "APACHE LICENSE, VERSION 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :main ^:skip-aot asynchronous-entities.core
   :target-path "target/%s"
