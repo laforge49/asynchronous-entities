0ff7550be3b71485ac289a7ae73421287ce2e173
Bill La Forge <laforge49@gmail.com>
2021-06-18 21:50:49 -0400

    Update core.clj

--- src/ae/core.clj
+++ src/ae/core.clj
@@ -14,30 +14,28 @@
         [(chan) (volatile! {})]
         env
         (atom {})]
-    (swap! env assoc :CONTEXTS {:CONTEXT_MAIN main-context})
+    (swap! env assoc :CONTEXTS {:CONTEXT/MAIN main-context})
     (vswap! (second main-context) (fn [old]
                                     (let [context-value
                                           (-> old
-                                              (assoc :NAME "CONTEXT_MAIN")
-                                              (assoc :ENTITIES {:SIMPLE_1 simple1
-                                                                :SIMPLE_2 simple2})
+                                              (assoc :NAME "CONTEXT/MAIN")
+                                              (assoc :ENTITIES {:MAIN/SIMPLE_1 simple1
+                                                                :MAIN/SIMPLE_2 simple2})
                                               )]
                                       context-value)))
     (vswap! (second simple1) (fn [old]
                                (let [context-value
                                      (-> old
-                                         (assoc :NAME "SIMPLE_1")
-                                         (assoc :CONTEXTS [:CONTEXT_MAIN])
-                                         (assoc :CHILDVECTORS {:PLAIN [:SIMPLE_2]})
+                                         (assoc :NAME "MAIN/SIMPLE_1")
+                                         (assoc :CHILDVECTORS {:PLAIN [:MAIN/SIMPLE_2]})
                                          )]
                                  context-value)))
     (vswap! (second simple2) (fn [old]
                                (let [context-value
                                      (-> old
-                                         (assoc :NAME "SIMPLE_2")
-                                         (assoc :CONTEXTS [:CONTEXT_MAIN])
-                                         (assoc :PARENTVECTORS {:PLAIN [:SIMPLE_1]})
+                                         (assoc :NAME "MAIN/SIMPLE_2")
+                                         (assoc :PARENTVECTORS {:PLAIN [:MAIN/SIMPLE_1]})
                                          )]
                                  context-value)))
-    (println (pr-str @(second (:SIMPLE_1 (get-in @(second (get-in @env [:CONTEXTS :CONTEXT_MAIN])) [:ENTITIES])))))
+    (println (pr-str @(second (:MAIN/SIMPLE_1 (get-in @(second (get-in @env [:CONTEXTS :CONTEXT/MAIN])) [:ENTITIES])))))
     ))
