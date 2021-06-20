c5c970493bb91d75529b211f5a5051827ca69772
Bill La Forge <laforge49@gmail.com>
2021-06-20 08:37:10 -0400

    create-entity

--- src/ae/core.clj
+++ src/ae/core.clj
@@ -7,39 +7,34 @@
   (a/go
     (let [main-out
           (a/<! main-in)
-          simple1
-          [(a/chan) (volatile! {})]
-          simple2
-          [(a/chan) (volatile! {})]
-          main-context
-          [(a/chan) (volatile! {})]
           env
-          {:CONTEXTS {:CONTEXT/MAIN main-context}
-           :PARAMS   {}}
+          {:CONTEXTS {}
+           :PARAMS   {:entity-map {:OPERATIONS {}
+                                   :CHILDVECTORS {}
+                                   :PARENTVECTORS {}}}}
+          simple1
+          (k/create-entity (assoc-in env [:PARAMS :entity-map :NAME] "MAIN/SIMPLE_1"))
+          simple2
+          (k/create-entity (assoc-in env [:PARAMS :entity-map :NAME] "MAIN/SIMPLE_2"))
+          main-context
+          (k/create-entity (-> env
+                               (assoc-in [:PARAMS :entity-map :NAME] "MAIN")
+                               (assoc-in [:PARAMS :entity-map :ENTITIES :MAIN/SIMPLE_1] simple1)
+                               (assoc-in [:PARAMS :entity-map :ENTITIES :MAIN/SIMPLE_2] simple2)
+                               ))
+          env
+          (assoc-in env [:CONTEXTS :CONTEXT/MAIN] main-context)
           ]
-      (vswap! (second main-context) (fn [old]
-                                      (let [context-value
-                                            (-> old
-                                                (assoc :NAME "CONTEXT/MAIN")
-                                                (assoc :ENTITIES {:MAIN/SIMPLE_1 simple1
-                                                                  :MAIN/SIMPLE_2 simple2})
-                                                (assoc :OPERATIONS {})
-                                                )]
-                                        context-value)))
       (vswap! (second simple1) (fn [old]
                                  (let [context-value
                                        (-> old
-                                           (assoc :NAME "MAIN/SIMPLE_1")
-                                           (assoc :OPERATIONS {})
-                                           (assoc :CHILDVECTORS {:PLAIN [:MAIN/SIMPLE_2]})
+                                           (assoc-in [:CHILDVECTORS :PLAIN] [:MAIN/SIMPLE_2])
                                            )]
                                    context-value)))
       (vswap! (second simple2) (fn [old]
                                  (let [context-value
                                        (-> old
-                                           (assoc :NAME "MAIN/SIMPLE_2")
-                                           (assoc :OPERATIONS {})
-                                           (assoc :PARENTVECTORS {:PLAIN [:MAIN/SIMPLE_1]})
+                                           (assoc-in [:PARENTVECTORS :PLAIN] [:MAIN/SIMPLE_1])
                                            )]
                                    context-value)))
       (a/>! main-out (pr-str @(second (:MAIN/SIMPLE_1 (get-in @(second (get-in env [:CONTEXTS :CONTEXT/MAIN])) [:ENTITIES])))))
--- src/ae/kernel.clj
+++ src/ae/kernel.clj
@@ -5,7 +5,16 @@
   (atom {}))
 
 (defn create-operation-port
-  [operation-kw env]
-  (let [port (a/chan)]
+  [env]
+  (let [operation-kw
+        (get-in env [:PARAMS :operation-kw])
+        port
+        (a/chan)]
     ((swap! operation-ports assoc operation-kw port))
     port))
+
+(defn create-entity
+  [env]
+  (let [entity-map (get-in env [:PARAMS :entity-map])]
+    [(a/chan)
+     (volatile! entity-map)]))
