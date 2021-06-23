5c57508e6b8ff55808491c8cd9ac039477875950
Bill La Forge <laforge49@gmail.com>
2021-06-23 17:29:05 -0400

    Update operations.clj

--- src/ae/operations.clj
+++ src/ae/operations.clj
@@ -27,9 +27,36 @@
             ]
         (vswap! contexts-volatile assoc-in [:CONTEXTS context-name-kw] new-context)
         (a/>! operation-return-port new-context)
-        (recur)
-        ))))
+        (recur)))))
+
+(defn create-entity-registration-operation
+  [env]
+  (let [entity-registration-port
+        (k/register-operation-port (assoc env :PARAMS {:operation-port-kw :REGISTER-ENTITY-PORT}))]
+    (a/go-loop []
+      (let [env
+            (a/<! entity-registration-port)
+            params
+            (:PARAMS env)
+            context-entity
+            (:master-entity env)
+            context-volatile
+            (second context-entity)
+            entity-name
+            (get-in env [:PARAMS :name])
+            [entity-name-kw _ _]
+            (kw/name-as-keyword entity-name)
+            new-entity
+            (k/create-entity env)
+            operation-return-port
+            (:operation-return-port params)
+            ]
+        (vswap! context-volatile assoc-in [:ENTITIES entity-name-kw] new-entity)
+        (a/>! operation-return-port new-entity)
+        (recur)))))
 
 (defn create-operations
   [env]
-  (create-context-registration-operation env))
+  (create-context-registration-operation env)
+  (create-entity-registration-operation env)
+  )
