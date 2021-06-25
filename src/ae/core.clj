977ceb4fe5e1b94df1448bc1002800ad63db95d4
Bill La Forge <laforge49@gmail.com>
2021-06-25 12:14:26 -0400

    Update core.clj

--- src/ae/core.clj
+++ src/ae/core.clj
@@ -14,7 +14,7 @@
   )
 
 (defn script1
-  [env]
+  [env params]
   [{:request         :REGISTER-CONTEXT-REQUEST
     :name            "CONTEXT/MAIN"
     :operation-ports {:REGISTER-ENTITY-REQUEST :REGISTER-ENTITY-PORT
@@ -25,14 +25,14 @@
     :target-context-name "CONTEXT/MAIN"
     :name                "MAIN/SIMPLE_1"
     :operation-ports     {}
-    :return-port         (get-in env [:PARAMS :return-port1])
+    :return-port         (:return-port1 params)
     }
    {:request             :ROUTE-TO-CONTEXT-REQUEST
     :target-request      :REGISTER-ENTITY-REQUEST
     :target-context-name "CONTEXT/MAIN"
     :name                "MAIN/SIMPLE_2"
     :operation-ports     {:ADD-PARENT-REQUEST :ADD-PARENT-PORT}
-    :return-port         (get-in env [:PARAMS :return-port2])
+    :return-port         (:return-port2 params)
     }
    ])
 
@@ -51,33 +51,34 @@
                                   :operation-ports {:REGISTER-CONTEXT-REQUEST :REGISTER-CONTEXT-PORT
                                                     :ROUTE-TO-CONTEXT-REQUEST :ROUTE-TO-CONTEXT-PORT}
                                   })
-      env
-      (assoc env :CONTEXTS-ENTITY contexts)
-      return-port1
-      (a/chan)
-      return-port2
-      (a/chan)
-      return-port3
-      (a/chan)
-      _ (doseq [request-params (script1 (assoc env :PARAMS {:return-port1 return-port1 :return-port2 return-port2}))]
-          (a/>! (first contexts)
-                (assoc env :PARAMS request-params)))
-      simple1
-      (a/<! return-port1)
-      simple2
-      (a/<! return-port2)
-      _ (a/>! (first simple2)
-              (assoc env :PARAMS {:request            :ADD-PARENT-REQUEST
-                                  :relationship       :BASIC
-                                  :parent-entity-name "MAIN/SIMPLE_1"
-                                  :return-port        return-port3
-                                  }))
-]
-(a/<! return-port3)
-(a/>! main-out @(second simple2))
-)
-)
-main-in ) )
+            env
+            (assoc env :CONTEXTS-ENTITY contexts)
+            return-port1
+            (a/chan)
+            return-port2
+            (a/chan)
+            return-port3
+            (a/chan)
+            _ (doseq [request-params (script1 env {:return-port1 return-port1
+                                                   :return-port2 return-port2})]
+                (a/>! (first contexts)
+                      (assoc env :PARAMS request-params)))
+            simple1
+            (a/<! return-port1)
+            simple2
+            (a/<! return-port2)
+            _ (a/>! (first simple2)
+                    (assoc env :PARAMS {:request            :ADD-PARENT-REQUEST
+                                        :relationship       :BASIC
+                                        :parent-entity-name "MAIN/SIMPLE_1"
+                                        :return-port        return-port3
+                                        }))
+            ]
+        (a/<! return-port3)
+        (a/>! main-out @(second simple2))
+        )
+      )
+    main-in))
 
 (defn -main
   [& args]
