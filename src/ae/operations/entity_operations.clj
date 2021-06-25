108f9a965339563134fd9d40e227f18a67693e60
Bill La Forge <laforge49@gmail.com>
2021-06-24 22:24:28 -0400

    Update entity_operations.clj

--- src/ae/operations/entity_operations.clj
+++ src/ae/operations/entity_operations.clj
@@ -5,7 +5,27 @@
 
 (defn create-add-parent-operation
   [env]
-  )
+  (let [add-parent-port
+        (k/register-operation-port (assoc env :PARAMS {:operation-port-kw :ADD-PARENT-PORT}))]
+    (a/go-loop []
+      (let [env
+            (a/<! add-parent-port)
+            params
+            (:PARAMS env)
+            this-entity
+            (:this-entity env)
+            this-volatile-map
+            (second this-entity)
+            parent-entity
+            (:parent-entity params)
+            relationship
+            (:relationship params)
+            operation-return-port
+            (:operation-return-port params)
+            ]
+
+        (a/>! operation-return-port true)
+        (recur)))))
 
 (defn create-entity-operations
   [env]
