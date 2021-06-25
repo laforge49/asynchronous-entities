e0a69fdb590c0d715f448f8c5b272658bba6bff8
Bill La Forge <laforge49@gmail.com>
2021-06-24 22:06:51 -0400

    Update entity_operations.clj

--- src/ae/operations/entity_operations.clj
+++ src/ae/operations/entity_operations.clj
@@ -3,6 +3,11 @@
             [ae.kernel :as k]
             [ae.keywords :as kw]))
 
+(defn create-add-parent-operation
+  [env]
+  )
+
 (defn create-entity-operations
   [env]
+  (create-add-parent-operation env)
   )
