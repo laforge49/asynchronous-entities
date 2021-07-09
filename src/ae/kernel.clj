89cb74bcd8cc4340983aad4cd930f55c009ef71b
Bill La Forge <laforge49@gmail.com>
2021-07-09 16:24:57 -0400

    broken

--- src/ae/kernel.clj
+++ src/ae/kernel.clj
@@ -31,6 +31,102 @@
 (defn create-operation-dispatcher
   [this-map]
   (a/go-loop [this-map this-map]
+    (recur (try
+             (let [this-request-port-stack
+                   (:REQUEST-PORT-STACK this-map)
+                   _ (if (nil? this-request-port-stack)
+                       (throw (Exception. "This request port stack is nil")))
+                   this-request-port
+                   (peek this-request-port-stack)
+                   _ (if (nil? this-request-port)
+                       (throw (Exception. "This request port is nil")))
+                   request
+                   (a/<! this-request-port)
+                   _ (if (not (vector? request))
+                       (throw (Exception. (prn-str "Request is not a vector: " request))))
+                   [env params]
+                   request
+                   return-port
+                   (:return-port params)
+                   _ (if (nil? return-port)
+                       (throw (Exception. (prn-str "Return port is nil for params: " params))))]
+               (try
+                 (let [env
+                       (assoc env :this-map this-map)
+                       env
+                       (assoc env :active-request-port this-request-port)
+                       requestid
+                       (:requestid params)
+                       _ (if (nil? requestid)
+                           (throw (Exception. (prn-str "Requestid port is nil for params: " params))))
+                       this-descriptors
+                       (:DESCRIPTORS this-map)
+                       _ (if (nil? this-descriptors)
+                           (throw (Exception. (prn-str "Descriptors is nil"))))
+                       this-operation-portids
+                       (:OPERATION-PORTIDS this-descriptors)
+                       _ (if (nil? this-operation-portids)
+                           (throw (Exception. (prn-str "Operation portids is nil"))))
+                       [this-map return-value]
+                       (case requestid
+
+                         :SNAPSHOT
+                         [this-map this-map]
+
+                         :PUSH-REQUEST-PORT
+                         (let [new-request-port
+                               (:new-request-port params)
+                               saved-map
+                               this-map
+                               this-map
+                               (assoc this-map :REQUEST-PORT-STACK (conj this-request-port-stack new-request-port))]
+                           [this-map saved-map])
+
+                         :POP-REQUEST-PORT
+                         (let [this-map
+                               (assoc this-map :REQUEST-PORT-STACK (pop this-request-port-stack))]
+                           [this-map this-map])
+
+                         :RESET
+                         (let [this-map
+                               (:saved-map params)]
+                           [this-map this-map])
+
+                         ;;DEFAULT
+                         (let [operation-port-id
+                               (requestid this-operation-portids)
+                               _ (if (nil? operation-port-id)
+                                   (throw (Exception. (prn-str "Operation portid is nil for this-map, params:" this-map params))))
+                               operation-port
+                               (operation-port-id @operation-port-map-atom)
+                               operation-return-port
+                               (a/chan)
+                               _ (if (nil? operation-port-id)
+                                   (throw (Exception. (str "Operation port id is nil for params " params))))
+                               _ (if (nil? operation-port)
+                                   (throw (Exception. (str "Operation port is nil for params " params))))
+                               _ (a/>! operation-port [env (assoc params :operation-return-port operation-return-port)])
+                               operation-return-value
+                               (a/<! operation-return-port)
+                               _ (if (not (vector? operation-return-value))
+                                   (throw (Exception. (str "Operation return value is not a vector: " operation-return-value))))
+                               _ (if (not= (count operation-return-value) 3)
+                                   (throw (Exception. (str "Operation return value is not a 3-tuple: " operation-return-value))))]
+                           operation-return-value)
+
+                         )]
+                   (a/>! return-port [nil return-value])
+                   this-map)
+                 (catch Exception e
+                   (a/>! return-port [e nil])
+                   this-map)))
+             (catch Exception e
+               (stacktrace/print-stack-trace e)
+               this-map)))))
+
+(defn create-operation-dispatcherX
+  [this-map]
+  (a/go-loop [this-map this-map]
     (let [this-request-port-stack
           (:REQUEST-PORT-STACK this-map)
           this-request-port
@@ -49,10 +145,10 @@
                 (:return-port params)
                 _ (if (nil? return-port)
                     (stacktrace/print-stack-trace (Exception. (prn-str "Return port is nil for params: " params))))
-                descriptors
+                this-descriptors
                 (:DESCRIPTORS this-map)
-                this-operation-portid-map
-                (:OPERATION-PORTIDS descriptors)
+                this-operation-portids
+                (:OPERATION-PORTIDS this-descriptors)
                 env
                 (assoc env :this-map this-map)
                 env
@@ -82,7 +178,7 @@
                   ;;DEFAULT
                   (let [
                         operation-port-id
-                        (requestid this-operation-portid-map)
+                        (requestid this-operation-portids)
                         _ (if (nil? operation-port-id)
                             (throw (Exception. (prn-str "Operation portid is nil for this-map, params:" this-map params))))
                         operation-port
@@ -90,7 +186,7 @@
                         operation-return-port
                         (a/chan)
 
-                        #_ (println (prn-str :???? params))
+                        #_(println (prn-str :???? params))
 
                         [this-map e return-value]
                         (if (nil? operation-port-id)
@@ -113,7 +209,9 @@
                                    (Exception. (str "Operation return value is not a 3-tuple: " operation-return-value))
                                    nil]
                                   operation-return-value)))))]
-                    [this-map e return-value]))]
+                    [this-map e return-value])
+
+                  )]
             (if (some? e)
               (a/>! return-port [e nil])
               (if (not= return-value :NO-RETURN)
