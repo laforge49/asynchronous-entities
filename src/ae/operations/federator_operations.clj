d1542373a392704025056cec9587e6281407d7cb
Bill La Forge <laforge49@gmail.com>
2021-07-15 11:13:47 -0400

    :RUN-FEDERATION-OPERATIONID

--- src/ae/operations/federator_operations.clj
+++ src/ae/operations/federator_operations.clj
@@ -6,7 +6,7 @@
 (defn create-run-federation-operation
   [env]
   (let [new-run-federation-port
-        (k/register-operation-port env {:operationid :RUN-FEDERATION-PORTID})]
+        (k/register-operation-port env {:operationid :RUN-FEDERATION-OPERATIONID})]
     (a/go-loop []
       (let [[env params]
             (a/<! new-run-federation-port)
