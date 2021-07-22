e7d43338a2d1c086c15fcc4e426371652c853b72
Bill La Forge <laforge49@gmail.com>
2021-07-22 09:16:49 -0400

    Update entity_operations.clj

--- src/ae/operations/entity_operations.clj
+++ src/ae/operations/entity_operations.clj
@@ -4,104 +4,6 @@
             [ae.kernel :as k]
             [ae.keywords :as kw]))
 
-(defn addParentFunction
-  [env this-map params]
-  (let [this-name
-        (:NAME this-map)
-        parent-name
-        (:parent-name params)
-        relationship
-        (:relationship params) relationship-parents
-        (get-in this-map [:PARENTVECTORS relationship] [])
-        _ (if (> (.indexOf relationship-parents parent-name) -1)
-            (throw (Exception. (str "Entity " parent-name " is already a " relationship
-                                    " parent of " this-name))))
-        relationship-parents
-        (conj relationship-parents parent-name)
-        this-map
-        (assoc-in this-map [:PARENTVECTORS relationship] relationship-parents)]
-    [this-map this-map]))
-
-(defn addParentParams
-  [env this-map params]
-  {:target-requestid :ADD-PARENT-REQUESTID
-   :target-name      (:child-name params)
-   :parent-name      (:NAME this-map)
-   :relationship     (:relationship params)})
-
-(defn addChildOperation
-  [env this-map params]
-  (let [this-name
-        (:NAME this-map)
-        child-name
-        (:child-name params)
-        _ (if (not (k/federated? this-map))
-            (throw (Exception. (str "Entity " this-name
-                                    " is not federated and so can not add a relationship to "
-                                    child-name))))
-        relationship
-        (:relationship params)
-        relationship-children
-        (get-in this-map [:CHILDVECTORS relationship] [])
-        _ (if (> (.indexOf relationship-children child-name) -1)
-            (throw (Exception. (str "Entity " child-name " is already a " relationship
-                                    " child of " this-name))))
-        relationship-children
-        (conj relationship-children child-name)]
-    (assoc-in this-map [:CHILDVECTORS relationship] relationship-children)))
-
-(defn addRelationshipFunction
-  [env this-map params]
-  (let [this-map
-        (addChildOperation env this-map params)
-        [this-map rv]
-        (k/federationRouteFunction env this-map (addParentParams env this-map params))]
-    [this-map this-map]))
-
-(defn addRelationshipParams
-  [env this-map params]
-  {:target-requestid :ADD-RELATIONSHIP-REQUESTID
-   :target-name      (:NAME this-map)
-   :relationship     (:relationship params)
-   :child-name       (:child-name params)})
-
-(defn instantiateParams
-  [env this-map params]
-  {:target-requestid :INSTANTIATE-REQUESTID
-   :target-name      (:prototype params)
-   :name             (:child-name params)})
-
-(defn create-add-new-child-operation
-  [env]
-  (let [add-new-child-port
-        (k/register-operation env {:operationid :ADD-NEW-CHILD-OPERATIONID
-                                   :function    nil})]
-    (a/go-loop []
-      (let [[env this-map params]
-            (a/<! add-new-child-port)
-            operation-return-port
-            (:operation-return-port params)]
-        (try
-          (let [context-request-port
-                (:CONTEXT-REQUEST-PORT env)
-                instantiate-return-port
-                (a/chan)
-                add-relationship-return-port
-                (a/chan)
-                _ (a/>! context-request-port [env (into (instantiateParams env this-map params)
-                                                        {:return-port instantiate-return-port
-                                                         :requestid   :ROUTE-REQUESTID})])
-                _ (k/request-exception-check (a/<! instantiate-return-port))
-                _ (a/>! context-request-port [env (into (addRelationshipParams env this-map params)
-                                                        {:return-port add-relationship-return-port
-                                                         :requestid   :ROUTE-REQUESTID})])
-                this-map
-                (k/request-exception-check (a/<! add-relationship-return-port))]
-            (a/>! operation-return-port [this-map nil this-map]))
-          (catch Exception e
-            (a/>! operation-return-port [this-map e nil]))))
-      (recur))))
-
 (defn instantiateFunction
   [env this-map params]
   (let [new-entity-name
@@ -184,12 +86,65 @@
             (a/>! operation-return-port [this-map e nil]))))
       (recur))))
 
+(defn addParentFunction
+  [env this-map params]
+  (let [this-name
+        (:NAME this-map)
+        parent-name
+        (:parent-name params)
+        relationship
+        (:relationship params) relationship-parents
+        (get-in this-map [:PARENTVECTORS relationship] [])
+        _ (if (> (.indexOf relationship-parents parent-name) -1)
+            (throw (Exception. (str "Entity " parent-name " is already a " relationship
+                                    " parent of " this-name))))
+        relationship-parents
+        (conj relationship-parents parent-name)
+        this-map
+        (assoc-in this-map [:PARENTVECTORS relationship] relationship-parents)]
+    [this-map this-map]))
+
+(defn addParentParams
+  [env this-map params]
+  {:target-requestid :ADD-PARENT-REQUESTID
+   :target-name      (:child-name params)
+   :parent-name      (:NAME this-map)
+   :relationship     (:relationship params)})
+
+(defn addChildOperation
+  [env this-map params]
+  (let [this-name
+        (:NAME this-map)
+        child-name
+        (:child-name params)
+        _ (if (not (k/federated? this-map))
+            (throw (Exception. (str "Entity " this-name
+                                    " is not federated and so can not add a relationship to "
+                                    child-name))))
+        relationship
+        (:relationship params)
+        relationship-children
+        (get-in this-map [:CHILDVECTORS relationship] [])
+        _ (if (> (.indexOf relationship-children child-name) -1)
+            (throw (Exception. (str "Entity " child-name " is already a " relationship
+                                    " child of " this-name))))
+        relationship-children
+        (conj relationship-children child-name)]
+    (assoc-in this-map [:CHILDVECTORS relationship] relationship-children)))
+
+(defn addRelationshipFunction
+  [env this-map params]
+  (let [this-map
+        (addChildOperation env this-map params)
+        [this-map rv]
+        (k/federationRouteFunction env this-map (addParentParams env this-map params))]
+    [this-map this-map]))
+
 (defn create-entity-operations
   [env]
+  (create-instantiate-operation env)
   (k/register-function env {:operationid :ADD-PARENT-OPERATIONID
                             :function    addParentFunction})
   (k/register-operation env {:operationid :ADD-RELATIONSHIP-OPERATIONID
                              :function    addRelationshipFunction})
-  (create-add-new-child-operation env)
-  (create-instantiate-operation env)
   )
