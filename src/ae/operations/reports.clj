f5820cf3811d06aafe7d59c3e8a8aeaa43468ef0
Bill La Forge <laforge49@gmail.com>
2021-08-04 11:59:31 -0400

    Update reports.clj

--- src/ae/operations/reports.clj
+++ src/ae/operations/reports.clj
@@ -3,6 +3,53 @@
             [ae.kernel :as k]
             [ae.keywords :as kw]))
 
+(defn short-names
+  [name-kws default-context-name]
+  (reduce
+    (fn [sorted-names name-kw]
+      (let [[name context-base-name base-name]
+            (kw/keyword-as-name name-kw)
+            name2
+            (if (= default-context-name context-base-name)
+              [(str "+" base-name) name]
+              [name name])]
+        (conj sorted-names name2)))
+    (sorted-set)
+    name-kws))
+
+(defn classifier-report
+  [n this-name this-map]
+  (let [[this-name-kw this-context-base-name this-base-name]
+        (kw/name-as-keyword this-name)
+        classifiers
+        (keys (:CLASSIFIERS this-map))
+        sorted-classifier-name2s
+        (short-names classifiers this-context-base-name)
+        lines
+        (reduce
+          (fn [lines [short-name name]]
+            (let [[name-kw context-base-name base-name]
+                  (kw/name-as-keyword name)
+                  value
+                  (get-in this-map [:CLASSIFIERS name-kw])
+                  [value-kw context-base-name base-name]
+                  (kw/name-as-keyword value)
+                  short-value
+                  (if (= this-context-base-name context-base-name)
+                    (str "+" base-name)
+                    value)]
+              (conj lines (str short-name " = " short-value "\n"))))
+          []
+          sorted-classifier-name2s)
+        nbr
+        (count sorted-classifier-name2s)]
+    (str n ". Classifiers of entity " this-name "\n"
+         "(Default context is " this-context-base-name ".)\n\n"
+         (s/join lines)
+         (if (> nbr 0)
+           "\n")
+         "Number of classifiers: " nbr "\n\n")))
+
 (defn descriptor-report
   [n this-name this-map]
   (let [[this-name-kw this-context-base-name this-base-name]
