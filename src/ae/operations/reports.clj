644868888d57fd8ff9936c4dfae7508f9ef379a1
Bill La Forge <laforge49@gmail.com>
2021-08-04 13:40:32 -0400

    Update reports.clj

--- src/ae/operations/reports.clj
+++ src/ae/operations/reports.clj
@@ -4,18 +4,27 @@
             [ae.keywords :as kw]))
 
 (defn short-names
-  [name-kws default-context-name]
+  [names-kws default-context-name]
   (reduce
-    (fn [sorted-names name-kw]
-      (let [[name context-base-name base-name]
-            (kw/keyword-as-name name-kw)
-            name2
-            (if (= default-context-name context-base-name)
-              [(str "+" base-name) name]
-              [name name])]
-        (conj sorted-names name2)))
+    (fn [sorted-names name-or-kw]
+      (if (keyword? name-or-kw)
+        (let [[name context-base-name base-name]
+              (kw/keyword-as-name name-or-kw)
+              name2
+              (if (= default-context-name context-base-name)
+                [(str "+" base-name) name]
+                [name name])]
+          (conj sorted-names name2))
+        (let [[name-kw context-base-name base-name]
+              (kw/name-as-keyword name-or-kw)
+              name2
+              (if (= default-context-name context-base-name)
+                [(str "+" base-name) name]
+                [name name])]
+          (conj sorted-names name2))
+        ))
     (sorted-set)
-    name-kws))
+    names-kws))
 
 (defn classifier-report
   [n this-name this-map]
@@ -102,19 +111,19 @@
         (kw/name-as-keyword this-name)
         classifier-registry
         (k/get-classifier-values-map this-name)
-        classifier-names
+        classifier-kws
         (keys classifier-registry)
         sorted-classifier-name2s
-        (short-names classifier-names this-base-name)
+        (short-names classifier-kws this-base-name)
         lines
         (reduce
           (fn [lines [short-classifier-name classifier-name]]
-            (let [values-map
-                  ((first (kw/name-as-keyword classifier-name)) classifier-registry)
-                  line
+            (let [line
                   (str "classifier:    " short-classifier-name "\n")
                   lines
                   (conj lines line)
+                  values-map
+                  ((first (kw/name-as-keyword classifier-name)) classifier-registry)
                   lines
                   (reduce
                     (fn [lines [classifier-value entity-names]]
