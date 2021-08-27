fe6bd3ed4f2471a275d62a86f100f8e7dff20f86
Bill La Forge <laforge49@gmail.com>
2021-08-27 11:06:44 -0400

    Refactored core

--- src/ae/core.clj
+++ src/ae/core.clj
@@ -13,60 +13,55 @@
   [env]
   (co/create-context-operations env)
   (eo/create-entity-operations env)
-  (fo/create-federator-operations env)
-  )
+  (fo/create-federator-operations env))
 
-(defn a-main
-  []
-  (let [main-in
+(defn async-script
+  [yaml-script env]
+  (let [out
         (a/chan)]
     (a/go
-      (let [main-out
-            (a/<! main-in)]
-        (try
-          (let [env
-                {}
-                _ (create-operations env)
-                context-request-port
-                (first (k/create-entity env
-                                        {"name"        "SYS"
-                                         "descriptors" {"SYS+REQUESTID_MAP" {"SYS+REGISTER_ENTITYrequestid"     ["REGISTER_ENTITYoperationid"]
-                                                                             "SYS+ROUTErequestid"               ["ROUTEoperationid"]
-                                                                             "SYS+REGISTER_CLASSIFIERrequestid" ["REGISTER_CLASSIFIERoperationid"]
-                                                                             "SYS+ENTITY_REPORTrequestid"       ["CONTEXT_REPORToperationid"]}}}))
-                env
-                (assoc env "CONTEXT-REQUEST-PORT" context-request-port)
-                boot-script-path
-                "./scripts/boot-script.yml"
-                yaml-script
-                (slurp boot-script-path)
-                edn-script
-                (yaml/parse-raw yaml-script)
-                return-port0
-                (a/chan)
-                _ (doseq [request-params edn-script]
-                    (let [request-params
-                          (assoc request-params "requestid" "SYS+ROUTErequestid")
-                          request-params
-                          (assoc request-params "return_port" return-port0)]
-                      (a/>! context-request-port [env request-params])
-                      (k/request-exception-check (a/<! return-port0))))
-                ]
-            (a/>! main-out [nil []]))
-          (catch Exception e
-            (a/>! main-out [e nil])))))
-    main-in))
+      (try
+        (let [edn-script
+              (yaml/parse-raw yaml-script)
+              return-port0
+              (a/chan)
+              context-request-port
+              (get env "CONTEXT-REQUEST-PORT")]
+          (doseq [request-params edn-script]
+            (let [request-params
+                  (assoc request-params "requestid" "SYS+ROUTErequestid")
+                  request-params
+                  (assoc request-params "return_port" return-port0)]
+              (a/>! context-request-port [env request-params])
+              (k/request-exception-check (a/<! return-port0)))))
+        (a/>! out [nil])
+        (catch Exception e
+          (a/>! out [e]))))
+    out))
 
 (defn -main
   [& args]
   (println "go!")
   (try
-    (let [main-in
-          (a-main)
-          main-out
-          (a/chan)]
-      (a/>!! main-in main-out)
-      (doseq [s (k/request-exception-check (a/<!! main-out))]
-        (println "\n" (pr-str s))))
+    (let [env
+          {}
+          _ (create-operations env)
+          context-request-port
+          (first (k/create-entity env
+                                  {"name"        "SYS"
+                                   "descriptors" {"SYS+REQUESTID_MAP" {"SYS+REGISTER_ENTITYrequestid"     ["REGISTER_ENTITYoperationid"]
+                                                                       "SYS+ROUTErequestid"               ["ROUTEoperationid"]
+                                                                       "SYS+REGISTER_CLASSIFIERrequestid" ["REGISTER_CLASSIFIERoperationid"]
+                                                                       "SYS+ENTITY_REPORTrequestid"       ["CONTEXT_REPORToperationid"]}}}))
+          env
+          (assoc env "CONTEXT-REQUEST-PORT" context-request-port)
+          boot-script-path
+          "./scripts/boot-script.yml"
+          yaml-script
+          (slurp boot-script-path)
+          [e]
+          (a/<!! (async-script yaml-script env))]
+      (if (some? e)
+        (throw e)))
     (catch Exception e
       (stacktrace/print-stack-trace e))))
