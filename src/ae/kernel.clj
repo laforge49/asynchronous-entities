e1f7f051260b57fd95a41a5af762b0494ff9adca
Bill La Forge <laforge49@gmail.com>
2021-11-28 14:41:28 -0500

    Update kernel.clj

--- src/ae/kernel.clj
+++ src/ae/kernel.clj
@@ -320,7 +320,6 @@
                   (assoc env "active-request-port" this-request-port)
                   requestid
                   (get params "SYS+param-REQUESTID")
-                  ;_ (println :dispatch requestid)
                   _ (if (nil? requestid)
                       (throw (Exception. (str "Requestid port is nil\n"
                                               (prn-str params)
