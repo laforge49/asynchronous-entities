81a00970a321ebb9e19be0560fe15f9c0f7d31b1
Bill La Forge <laforge49@gmail.com>
2021-12-09 13:06:20 -0500

    Update later.clj

--- src/ae/later.clj
+++ src/ae/later.clj
@@ -13,39 +13,43 @@
 (defn go-later
   [env requests]
   (let [requeststackatom
-        (atom [seq requests])
+        (atom [(seq requests)])
         env
         (assoc env "SYS+env_atmvec-requeststackatom$seq" requeststackatom)]
     (swap! active-count-atom inc)
-    (a/go-loop [[more env requests] [true env requests]]
-      (if more
-        (if (empty? @requeststackatom)
-          (when (= (swap! active-count-atom dec) 0)
-            (reset! live-atom false)
-            (a/>! exit-chan [nil]))
-          (if (nil? requests)
+    (a/go-loop []
+      (if @live-atom
+        (let [requeststack
+              @requeststackatom]
+          (if (empty? requeststack)
             (when (= (swap! active-count-atom dec) 0)
               (reset! live-atom false)
               (a/>! exit-chan [nil]))
-            (recur (try
-                     (let [request
-                           (first requests)
-                           params
-                           (get request "request_map-REQUEST^param")
-                           requests
-                           (next requests)
-                           target-name
-                           (get params "SYS+param-TARGETname&?")
-                           request-port
-                           (k/get-public-request-port target-name)
-                           subrequest-return-port
-                           (a/chan)
-                           params
-                           (assoc params "SYS+param-RETURN$chan" subrequest-return-port)]
-                       (a/>! request-port [env params])
-                       (k/request-exception-check (a/<! subrequest-return-port))
-                       [@live-atom env requests])
-                     (catch Exception e
-                       (a/>! exit-chan [e])
-                       (reset! live-atom false)
-                       [false nil nil])))))))))
+            (do
+              (try
+                (let [requests
+                      (peek requeststack)
+                      _ (swap! requeststackatom pop)]
+                  (if (not (empty? requests))
+                    (let [request
+                          (first requests)
+                          nextrequests
+                          (next requests)
+                          _ (if (some? nextrequests)
+                              (swap! requeststackatom conj (next requests)))
+                          params
+                          (get request "request_map-REQUEST^param")
+                          target-name
+                          (get params "SYS+param-TARGETname&?")
+                          request-port
+                          (k/get-public-request-port target-name)
+                          subrequest-return-port
+                          (a/chan)
+                          params
+                          (assoc params "SYS+param-RETURN$chan" subrequest-return-port)]
+                      (a/>! request-port [env params])
+                      (k/request-exception-check (a/<! subrequest-return-port)))))
+                (catch Exception e
+                  (a/>! exit-chan [e])
+                  (reset! live-atom false)))
+              (recur))))))))
