a2f35057af204d534332e4a2f43efa6cf577d0c1
Bill La Forge <laforge49@gmail.com>
2022-01-03 16:28:26 -0500

    Update core.clj

--- src/ae/core.clj
+++ src/ae/core.clj
@@ -6,14 +6,16 @@
             [ae.later :as l]
             [ae.operations.context-operations :as co]
             [ae.operations.entity-operations :as eo]
-            [ae.operations.federator-operations :as fo]))
+            [ae.operations.federator-operations :as fo]
+            [ae.operations.swing-operations :as sw]))
 
 
 (defn create-operations
   [env]
   (co/create-context-operations env)
   (eo/create-entity-operations env)
-  (fo/create-federator-operations env))
+  (fo/create-federator-operations env)
+  (sw/create-swing-operations env))
 
 (defn -main
   [& args]
