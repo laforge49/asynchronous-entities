f0901b64795b0af8530ef09b593670314f51f373
Bill La Forge <laforge49@gmail.com>
2022-01-05 14:57:35 -0500

    deploy merge-maps

--- src/ae/operations/entity_operations.clj
+++ src/ae/operations/entity_operations.clj
@@ -52,10 +52,8 @@
         (get this-map "SYS+facet_map-DESCRIPTORS^descriptor")
         instantiation-descriptors
         (get this-descriptors "SYS+descriptor_map-INSTANCE^descriptor")
-        ;instantiation-descriptors
-        ;(k/merge-maps instantiation-descriptors (get params "SYS+param_map-DESCRIPTORS^descriptor"))
         instantiation-descriptors
-        (into instantiation-descriptors (get params "SYS+param_map-DESCRIPTORS^descriptor"))
+        (k/merge-maps instantiation-descriptors (get params "SYS+param_map-DESCRIPTORS^descriptor"))
         instantiation-classifiers
         (get this-descriptors "SYS+descriptor_map-INSTANCE^classifier")
         instantiation-classifiers
