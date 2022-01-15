408a7a7a5b756eb3c99bf4389d443aa9c42e0e04
Bill La Forge <laforge49@gmail.com>
2022-01-15 07:09:52 -0500

    Update kernel.clj

--- src/ae/kernel.clj
+++ src/ae/kernel.clj
@@ -597,7 +597,7 @@
               (throw (Exception. (str "There is no name type for " (pr-str edn)))))
             (if (and (not= parent-ntyp "%") (not= typ parent-ntyp))
               (throw (Exception. (str (pr-str edn) " is not of name type " parent-ntyp))))
-            (if (nil? (get-entity-map edn))
+            (if (not (entity-exists? edn))
               (throw (Exception. (str "No such entity: " (pr-str edn)))))))))
 
     (vector? edn)
