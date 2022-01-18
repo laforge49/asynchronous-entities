2cae673c14309741fbe05ca93273800b835bd485
Bill La Forge <laforge49@gmail.com>
2022-01-18 10:45:30 -0500

    println-function

--- src/ae/operations/util_operations.clj
+++ src/ae/operations/util_operations.clj
@@ -2,19 +2,26 @@
   (:require [clojure.core.async :as a]
             [ae.kernel :as k]))
 
+(defn println-function
+  [env this-map params]
+  (println (get params "SYS+param-TEXT$str"))
+  [this-map nil])
+
 (defn println-goblock
   [env this-map params]
   (a/go
     (let [operation-return-port
           (get params "SYS+param-OPERATIONreturnport")]
       (try
-        (println (get params "SYS+param-TEXT$str"))
-        (a/>! operation-return-port [this-map nil])
+        (let [[this-map rv]
+              (println-function env this-map params)]
+          (a/>! operation-return-port [this-map nil]))
         (catch Exception e
           (a/>! operation-return-port [this-map e]))))))
 
 (defn create-util-operations
   [env]
   (k/register-function env {:operationid "PRINTLNoperationid"
+                            :function    println-function
                             :goblock     println-goblock})
   )
