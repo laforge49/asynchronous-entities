8a4f24eba40131b588a5cb0f4055c7dc2853ed4c
Bill La Forge <laforge49@gmail.com>
2022-02-18 08:02:22 -0500

    Update kernel.clj

--- src/ae/kernel.clj
+++ src/ae/kernel.clj
@@ -713,6 +713,24 @@
     true
     (throw (Exception. (str "Data type " (pr-str parent-dtyp) " is not known, value: " (pr-str edn))))))
 
+(defn merge-maps
+  [base-map update-map]
+  (reduce
+    (fn [base-map [uk uv]]
+      (let [bv
+            (get base-map uk)]
+        (if (nil? bv)
+          (assoc base-map uk uv)
+          (if (not= (map? bv) (map? uv))
+            (throw (Exception. (str "change of value type for " uk
+                                    "\n" :bv " " (map? bv) " " (prn-str bv)
+                                    :uv " " (map? uv) " " (prn-str uv))))
+            (if (not (map? uv))
+              (assoc base-map uk uv)
+              (assoc base-map uk (merge-maps bv uv)))))))
+    base-map
+    update-map))
+
 (def testChanClass (class (a/chan)))
 
 (defn unbind-context
@@ -774,24 +792,6 @@
 
     true
     (pr-str (class edn))))
-
-(defn merge-maps
-  [base-map update-map]
-  (reduce
-    (fn [base-map [uk uv]]
-      (let [bv
-            (get base-map uk)]
-        (if (nil? bv)
-          (assoc base-map uk uv)
-          (if (not= (map? bv) (map? uv))
-            (throw (Exception. (str "change of value type for " uk
-                                    "\n" :bv " " (map? bv) " " (prn-str bv)
-                                    :uv " " (map? uv) " " (prn-str uv))))
-            (if (not (map? uv))
-              (assoc base-map uk uv)
-              (assoc base-map uk (merge-maps bv uv)))))))
-    base-map
-    update-map))
 
 (defn bind-context-
   [local-context resources-set edn parent-styp parent-ktyp parent-ntyp parent-dtyp env]
